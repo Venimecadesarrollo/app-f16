80c6dd1f967bb0ee6a2afe279046453a54ff3c06
Oswaldo Alcalá <odarotto@gmail.com>
2024-10-26 16:28:43 -0400

    feat(registro): Agregando funcionalidad para formulario de registro

--- backend/urls.py
+++ backend/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("registro/", views.registro, name="registro"),
     path("calculadora/", views.calculadora, name="calculadora"),
+    path("process-register/", views.process_register, name="process_register"),
 ]
 
--- backend/views.py
+++ backend/views.py
@@ -1,5 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+import requests
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from backend.utils.requests_func import login
+from backend.forms import CargoTrackForm
+
+
+# ? Load environment variables
+load_dotenv()
+
 
 # Create your views here.
 def index(request):
@@ -12,3 +24,45 @@ def registro(request):
 
 def calculadora(request):
     return render(request, "calculadora.html")
+
+
+def process_register(request):
+    session: requests.Session = requests.Session()
+    user = os.getenv("CARGOTRACK_USER")
+    password = os.getenv("CARGOTRACK_PASS")
+    email = os.getenv("CARGOTRACK_EMAIL")
+
+    if request.method == "POST":
+        form = CargoTrackForm(request.POST)
+
+        if form.is_valid():
+            # ! If form is valid, send data to CargoTrack
+            data = form.cleaned_data
+            print(data)
+
+            # ? Try to login 3 times
+            max_attempts = 3
+            attempts = 0
+            login_response = None
+
+            while attempts < max_attempts:
+                login_response: requests.Response = login(user, password, session)
+                if login_response.status_code == 200:
+                    break
+                attempts += 1
+
+            # ! If no good response, return 500 response
+            if login_response.status_code != 200:
+                return HttpResponse("Login failed after multiple attempts")
+
+            # ? Send data to CargoTrack
+            # ? Use the same session to keep the login
+            data["button3"] = "Enviar"
+            data["action"] = "search"
+            data["consignee"] = "Y"
+            data["email"] = email
+            creation_response = session.post("https://bva.cargotrack.net/appl2.0/agent/accounts_add.asp", data=data, allow_redirects=True)
+        else:
+            return HttpResponse(form.errors, status=400)
+
+    return HttpResponse()
